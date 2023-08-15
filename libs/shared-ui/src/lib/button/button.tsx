e84e1e648b744ab298d9f21ac4ba9f0fdf89ab5d
malvin_jay <george@plendify.com>
2023-08-15 22:32:11 +0000

    Added shared components library

---
+++ libs/shared-ui/src/lib/button/button.tsx
@@ -0,0 +1,67 @@
+import { ButtonHTMLAttributes, forwardRef } from 'react';
+import { SpinnerIcon } from '@tasks-management/icons';
+import { helpers } from '@tasks-management/utils';
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isSubmitting?: boolean;
+  contentClassName?: string;
+}
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      children,
+      disabled,
+      className,
+      isSubmitting = false,
+      contentClassName,
+      ...props
+    },
+    ref
+  ) => {
+    /**
+     * variables
+     */
+    disabled = (() => {
+      if (isSubmitting) {
+        return true;
+      }
+      return disabled;
+    })();
+
+    return (
+      <button
+        ref={ref}
+        className={helpers.classNames(
+          'outline-0',
+          'text-sm font-semibold whitespace-nowrap select-none',
+          'rounded-md hover:bg-opacity-80',
+          'gap-2 flex items-center',
+          'disabled:pointer-events-none',
+          'active:shadow-[inset_0_0_100px_100px_rgba(0,0,0,0.1)]',
+          className
+        )}
+        {...{ disabled, ...props }}
+      >
+        {isSubmitting ?
+        <svg className="animate-spin 1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-Width="4"></circle>
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        :
+        ''
+        }
+        <div
+          className={helpers.classNames(
+            contentClassName,
+            'flex items-center justify-center gap-2'
+          )}
+        >
+          {children}
+        </div>
+      </button>
+    );
+  }
+);
+
+export default Button;
